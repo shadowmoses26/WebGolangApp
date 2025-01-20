b559dae7c960e1a08290e271977c15cdc37e0c95
shadowmoses26 <dracula094@mail.ru>
2025-01-20 22:03:34 +0300

    Add template processor.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"fmt"
-	_ "fmt"
 	"log"
 	"net/http"
-	"strconv"
-	_ "strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-
-		http.Error(w, "Метод запрещен", 405)
-		return
-
-	}
-
-	w.Write([]byte("Создать сниппет"))
-}
-
 func main() {
-
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Запускаем сервер: http://127.0.0.1:4000 ")
+	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-
 }
